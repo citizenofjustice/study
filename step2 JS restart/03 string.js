fd915fd7bfc30525eaafdddb1211b2ddfae7e157
CinemaAddict <brokenswords21@gmaile.com>
2022-03-12 13:38:36 +0300

    commit

---
+++ step2 JS restart/03 string.js
@@ -0,0 +1,51 @@
+const firstName = 'Андрей'
+// const age = 26
+
+// function getAge() {
+//     return age
+// }
+// const output = 'Привет, меня зовут ' + firstName + ' и мой возраст ' + age + 'лет.'
+// const output = `Привет, меня зовут ${firstName} и мой возраст ${getAge()} лет.`
+// const output = `Привет, меня зовут ${firstName} и мой возраст ${age < 20 ? '<20>': '>20'} лет.`
+// const output = `
+//     <div>This is div</div>
+//     <p>this is 0</p>
+// `
+// console.log(output)
+
+// console.log(firstName.length)
+// console.log(firstName.toUpperCase())
+// console.log(firstName.toLowerCase())
+// console.log(firstName.charAt(2))
+// console.log(firstName.indexOf('др')) //поиск с какого символа начинается данная подстрока. первый символ имеет 0 индекс
+// console.log(firstName.indexOf('влад')) // -1 значит что такой подстроки нет
+// console.log(firstName.startsWith('Анд'))
+// console.log(firstName.startsWith('анд'))
+// console.log(firstName.toLowerCase().startsWith('анд'))
+// console.log(firstName.repeat(3))
+
+// const string = '    password     '
+
+// console.log(string)
+// console.log(string.trim())
+// console.log(string.trimLeft())
+
+
+// function logPerson(s, name, age) {
+//     console.log(s, name, age)
+//     if (age < 0) {
+//         age = 'еще не родился'
+//     }
+//     // return 'Info about person'
+//     return `${s[0]}${name}${s[1]}${age}${s[2]}`
+// }
+
+// const personName = 'Андрей'
+// const personName2 = 'Максим'
+// const personAge = 26
+// const personAge2 = -8
+
+// const output = logPerson`Имя: ${personName}, Возраст: ${personAge}`
+// const output2 = logPerson`Имя: ${personName2}, Возраст: ${personAge2}`
+// console.log(output)
+// console.log(output2)
