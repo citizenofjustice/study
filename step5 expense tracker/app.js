7a15894788d3df65608f8a6a9b00b0df3d55e1bb
CinemaAddict <brokenswords21@gmaile.com>
2022-05-11 19:10:07 +0300

    commit

--- step5 expense tracker/app.js
+++ step5 expense tracker/app.js
@@ -1,14 +1,20 @@
-const name = document.getElementsByClassName('name');
-const date = document.getElementsByClassName('date');
+const expenseName = document.getElementsByClassName('name');
+const expenseDate = document.getElementsByClassName('date');
 const amount = document.getElementsByClassName('amount');
+const form = document.getElementsByClassName('textFields');
 
 function logSubmit(e) {
     e.preventDefault(); 
-    addExpense();
+    if (task.value.length > 0) {
+        addExpense()
+    }
 }
 
 function addExpense() {
-    
+    console.log(expenseName.value);
+    console.log(expenseDate.value);
+    console.log(expenseAmount.value);
+
 }
 
 form.addEventListener('submit', logSubmit); //ловим событие отправки form
