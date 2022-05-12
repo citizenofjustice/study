50bb27475b59c4a37fe739c81ad8f0043e8dc3fb
CinemaAddict <brokenswords21@gmaile.com>
2022-05-12 20:08:59 +0300

    commit

--- step5 expense tracker/app.js
+++ step5 expense tracker/app.js
@@ -1,20 +1,73 @@
-const expenseName = document.getElementsByClassName('name');
-const expenseDate = document.getElementsByClassName('date');
-const amount = document.getElementsByClassName('amount');
-const form = document.getElementsByClassName('textFields');
+const expenseName = document.getElementById('name');
+const expenseDate = document.getElementById('date');
+const expenseAmount = document.getElementById('amount');
+const form = document.getElementById('textFields');
+const expenseTable = document.getElementById('resultTable');
+const tableTitles = document.querySelector('.titleRow');
+
 
 function logSubmit(e) {
     e.preventDefault(); 
-    if (task.value.length > 0) {
-        addExpense()
-    }
+    addExpense();
+}
+
+//функция изменения формата даты (можно улучшить чтобы возвращался тип date)
+function formatDate(x) {
+    var parts = x.split("-");
+    var ddMmYyyy = new String(parts[2]+'.'+parts[1]+'.'+parts[0]);
+    return(ddMmYyyy);
 }
 
 function addExpense() {
-    console.log(expenseName.value);
-    console.log(expenseDate.value);
-    console.log(expenseAmount.value);
+    var table = expenseTable.lastChild.children.length;
+    console.log(table);
+
+
+    // ready(table);
+    // emptyMsg.parentNode.remove();
+    var newRow = expenseTable.insertRow(-1);
+    var column1 = newRow.insertCell(0);
+    var column2 = newRow.insertCell(1);
+    var column3 = newRow.insertCell(2);
+    column1.setAttribute('class', 'nameColumn');
+    column2.setAttribute('class', 'dateColumn');
+    column3.setAttribute('class', 'amountColumn');
+    
+    column1.innerHTML = expenseName.value;
+    column2.innerHTML = new String (formatDate(expenseDate.value));
+    column3.innerHTML = expenseAmount.value;
+
+
+    var del = document.createElement('span');  //создаем кнопку удаления
+    del.innerHTML = '<i class="material-symbols-outlined">delete</i>'; //заполняем ее значением
+    del.setAttribute('class', 'material-symbols-outlined');
+    newRow.insertAdjacentElement('beforeend', del)
+    // var row = document.createElement('tr');
+    // row.setAttribute('id', 'row'+rowCounter);
+
+    // var column1 = document.createElement('td');
+    // var column2 = document.createElement('td');
+    // var column3 = document.createElement('td');
+    // column1.setAttribute('class', 'nameColumn');
+    // column2.setAttribute('class', 'dateColumn');
+    // column3.setAttribute('class', 'amountColumn');
+
+    // expenseTable.insertAdjacentElement('beforeend', row);
+    // row.insertCell
+
+    // expenseName.value = column1;
 
 }
 
+function ready() {
+    if (tableTitles.parentNode.childElementCount == 1) {
+        var longRow = expenseTable.insertRow(-1);
+        var emptyMsg = longRow.insertCell(0);
+        emptyMsg.setAttribute('colspan', '3');
+        emptyMsg.innerHTML = '<p class="isCentered">В данный момент добавленных расходов нет, введите показатели выше и добавьте их в таблицу</p>';
+    }
+    
+}
+
+expenseTable.addEventListener("DOMContentLoaded", ready());
 form.addEventListener('submit', logSubmit); //ловим событие отправки form
