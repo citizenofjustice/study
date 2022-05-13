e43882da48d297bfad3b8005502f514b78a37635
CinemaAddict <brokenswords21@gmaile.com>
2022-05-13 21:29:57 +0300

    commit

--- step6 note app/app(scroll problem).js
+++ step6 note app/app(scroll problem).js
@@ -1,39 +1,38 @@
 const noteInput = document.getElementById('noteInput');
 const noteGrid = document.querySelector('.noteGrid');
-// const tmpDiv = document.querySelector('.tmpDiv');
 const form = document.querySelector('.noteText');
+// const sourceHeight = getHeight(noteInput);
 
 
 function elastic(element) {
     element.style.height = '5px';
     element.style.height = (element.scrollHeight)+"px";
-    var currentHeight = element.style.height;   
-    if (currentHeight == '54px') {
+    var sourceHeight = getHeight(element);
+    var currentHeight = element.style.height;
+    console.log(sourceHeight);
+    // var oY = sourceHeight.toString();
+    // console.log(oY);
+    console.log(currentHeight != sourceHeight);
+    console.log(currentHeight);
+    console.log(sourceHeight);
+    console.log(typeof(currentHeight));
+    console.log(typeof(sourceHeight));
+    
+    
+    if (currentHeight === sourceHeight) {
         element.style.overflowY = "hidden";
-    } else element.style.overflowY = "scroll"; 
+    }// else element.style.overflowY = "scroll"; 
 }
 
-function showfullNote(x) {
-    var textParent = x.parentElement;
-    var text = textParent.parentNode;
-    var popupNote = text.querySelector('.overflow-ellipsis').innerHTML;
-    console.log(popupNote);
-    
-    var back = document.body.style;
-    back.backgroundColor = '#c0c0c0';
-    back.opacity = '0.5';
-    back.
+function getHeight(e) {
+    return e.style.height;
 }
 
 function addNote() {
-    if (noteGrid.parentElement.firstElementChild != noteGrid) {
-        noteGrid.parentElement.firstElementChild.remove()
-    }
     //создаем общий блок для заголовка и текста
     var newNote = document.createElement('div');
     newNote.setAttribute('class', 'tmpDiv');
     noteGrid.insertAdjacentElement('beforeend', newNote);
-    noteGrid.style.padding = '20px';
 
     //создаем заголовок
     var newP = document.createElement('p');
@@ -58,12 +57,7 @@ function addNote() {
     //создаем кнопку
     var newButton = document.createElement('button');
     buttonDiv.insertAdjacentElement('beforeend', newButton);
-    newButton.setAttribute('class', 'popupButton');
-    newButton.setAttribute('onclick', 'showfullNote(this)');
     newButton.innerHTML = 'Просмотреть';
-
-    noteInput.value = '';
-    noteInput.focus();
 }
 
 function logSubmit(x) {
@@ -71,15 +65,15 @@ function logSubmit(x) {
     addNote();
 }
 
-function empty() {
-    if (noteGrid.nextElementSibling === null) {
-        var emptyMsg = document.createElement('p');
-        emptyMsg.setAttribute('class', 'emptyMsg');
-        noteGrid.insertAdjacentElement('beforebegin', emptyMsg);
-        emptyMsg.innerHTML = 'Добавленные заметки отсутствуют, вы можете ввести их через поле выше...';
-    }
-}
-
+// function empty() {
+//     if (tableTitles.nextElementSibling === null) {
+//         longRow = expenseTable.insertRow(-1);
+//         longRow.setAttribute('class', 'longRow');
+//         var emptyMsg = longRow.insertCell(0);
+//         emptyMsg.setAttribute('colspan', '3');
+//         emptyMsg.innerHTML = '<p class="isCentered">В данный момент добавленных расходов нет, введите показатели выше и добавьте их в таблицу</p>';
+//     }
+// }
+// document.addEventListener("DOMContentLoaded", getHeight(noteInput));
 document.addEventListener('DOMContentLoaded', elastic(noteInput));
-form.addEventListener('submit', logSubmit, empty());
-noteGrid.addEventListener('onclick', showfullNote(this));
+form.addEventListener('submit', logSubmit);
