30302ea3ae709cdabc407a66fe4dbb350eab8fab
CinemaAddict <brokenswords21@gmaile.com>
2022-06-01 21:57:10 +0300

    commit

--- step7 vowel counter/scripts/app.js
+++ step7 vowel counter/scripts/app.js
@@ -1,12 +1,35 @@
 $(document).ready(function(){
 
     //функция для передачи текста
-    $("button.button").click(function(){
-        var text = $("textarea.text").val();
-        // return text
-        $("textarea.result").append(text);
+    $("button.button").click(function(x){
+        // if ($("input:not(:empty)")) {
+        //     $("button").preventDefault();
+        //     // console.log('go on');
+        // }
         
+        // console.log(text);
+        
+        var text = $("textarea.text").val();
+        var words = text.split(" ");
+        var sentence = text.split(/\. + \! + \? /g); //+[\! ]+[\? ]
+        console.log(sentence);
+        
+        if ($("#symbols").is(":checked")) {
+            $("textarea.result").val("машина насчитала " + text.length + " символов");
+        }
+        
+        if ($("#words").is(":checked")) {
+            $("textarea.result").val("машина насчитала " + words.length + " слов");
+        }
+        
+        if ($("#sentence").is(":checked")) {
+            x.preventDefault();
+            $("textarea.result").val("машина насчитала " + sentence.length + " предложений");
+        }
+        
+        
+
     })
 
-    //функция подсчета символов
  });
+
