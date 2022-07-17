78aed0b07f67fc0be6c59e922ab2f1fa82d6ce54
CinemaAddict <brokenswords21@gmail.com>
2022-07-17 22:08:40 +0300

    commit

--- React-01-starting-setup/src/components/ExpenseItem.js
+++ React-01-starting-setup/src/components/ExpenseItem.js
@@ -1,16 +1,16 @@
 import "./ExpenseItem.css";
 
-function ExpenseItem() {
-  const expenseDate = new Date(2022, 2, 28);
-  const expenseTitle = "Страховка авто";
-  const expenseAmount = 4500;
+function ExpenseItem(props) {
+//   const expenseDate = new Date(2022, 2, 28);
+//   const expenseTitle = "Страховка авто";
+//   const expenseAmount = 4500;
 
   return (
     <div className="expense-item">
-      <div>{expenseDate.toISOString()}</div>
+      <div>{props.date.toISOString()}</div>
       <div className="expense-item__description">
-        <h2>{expenseTitle}</h2>
-        <div className="expense-item__price">{expenseAmount} руб.</div>
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">{props.amount} руб.</div>
       </div>
     </div>
   );
