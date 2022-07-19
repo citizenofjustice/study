a597f7bcc9b3e89c86d6b590e813817b132a1736
CinemaAddict <brokenswords21@gmail.com>
2022-07-19 22:07:18 +0300

    commit

--- React-01-starting-setup/src/components/Expenses/ExpenseItem.js
+++ React-01-starting-setup/src/components/Expenses/ExpenseItem.js
@@ -1,18 +1,27 @@
-import ExpenseDate from './ExpenseDate.js';
-import Card from '../UI/Card.js';
-import './ExpenseItem.css';
+import React, { useState } from "react";
 
+import ExpenseDate from "./ExpenseDate.js";
+import Card from "../UI/Card.js";
+import "./ExpenseItem.css";
 
 const ExpenseItem = (props) => {
+  const [title, setTitle] = useState(props.title);
+
+  const clickHandler = () => {
+    setTitle('Обновлено!');
+    console.log(title);
+  };
+
   return (
     <Card className="expense-item">
-      <ExpenseDate date={props.date}/>
+      <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">{props.amount} руб.</div>
       </div>
+      <button onClick={clickHandler}>Изменить название</button>
     </Card>
   );
-}
+};
 
 export default ExpenseItem;
