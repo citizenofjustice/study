00dce952a18a13b2dd04f70f30e6791505254dc1
CinemaAddict <brokenswords21@gmail.com>
2022-07-21 23:13:52 +0300

    commit

--- React-01-starting-setup/src/components/NewExpense/NewExpense.js
+++ React-01-starting-setup/src/components/NewExpense/NewExpense.js
@@ -2,10 +2,19 @@ import React from "react";
 import ExpenseForm from "./ExpenseForm.js";
 import "./NewExpense.css";
 
+
 const NewExpense = () => {
+  const saveExpenseDataHandler = (enteredExpenseData) => {
+    const expenseData ={
+      ...enteredExpenseData,
+      id: Math.random().toString()
+    };
+    console.log(expenseData);
+  };
+
   return (
     <div className="new-expense">
-      <ExpenseForm />
+      <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
     </div>
   );
 };
