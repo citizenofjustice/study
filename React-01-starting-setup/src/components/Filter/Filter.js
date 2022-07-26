67b1aa253eee732e0e09f2218c12378c34a1a1ac
CinemaAddict <brokenswords21@gmail.com>
2022-07-26 23:51:25 +0300

    commit

---
+++ React-01-starting-setup/src/components/Filter/Filter.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Filter = () => {
+  return (
+    <div className="filter-expenses">
+      <p>Заголовок</p>
+      <input className="filter-by-year"></input>
+      <div className="expense-diagramm"></div>
+    </div>
+  );
+};
+
+export default Filter;
