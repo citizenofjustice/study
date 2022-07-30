e27e16c05d50fedb66b26c1a0221e34419978cc9
CinemaAddict <brokenswords21@gmail.com>
2022-07-30 20:21:09 +0300

    commit

--- React-01-starting-setup/src/components/Filter/Filter.js
+++ React-01-starting-setup/src/components/Filter/Filter.js
@@ -1,8 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import Card from "../UI/Card.js";
 import "./Filter.css";
 
-const Filter = () => {
+const Filter = (props) => {
+  const [year, setYear] = useState(props.year);
+
+  const selectHandler = (event) => {
+    console.log(event.target.value);
+  };
+
   const years = [2022, 2021, 2020, 2019];
   const months = ['Янв.', 'Фев.',  'Мар.',  'Апр.',  'Май',  'Июнь',  'Июль',  'Авг.',  'Сент.',  'Окт.',  'Ноя.',  'Дек.'];
 
@@ -11,7 +17,7 @@ const Filter = () => {
       <span className="filter-by-year">
         <p>Фильтр по годам</p>
         <form>
-          <select defaultValue={years[0]}>
+          <select onChange={selectHandler} defaultValue={years[0]} >
             <option>{years[0]}</option>
             <option>{years[1]}</option>
             <option>{years[2]}</option>
