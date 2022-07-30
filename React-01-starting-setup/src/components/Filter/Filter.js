616efcad2f60b41a5e83febb94757f11f5b436ea
CinemaAddict <brokenswords21@gmail.com>
2022-07-30 19:27:00 +0300

    commit

--- React-01-starting-setup/src/components/Filter/Filter.js
+++ React-01-starting-setup/src/components/Filter/Filter.js
@@ -1,12 +1,52 @@
 import React from "react";
+import Card from "../UI/Card.js";
+import "./Filter.css";
 
 const Filter = () => {
+  const years = [2022, 2021, 2020, 2019];
+  const months = ['Янв.', 'Фев.',  'Мар.',  'Апр.',  'Май',  'Июнь',  'Июль',  'Авг.',  'Сент.',  'Окт.',  'Ноя.',  'Дек.'];
+
   return (
-    <div className="filter-expenses">
-      <p>Заголовок</p>
-      <input className="filter-by-year"></input>
-      <div className="expense-diagramm"></div>
-    </div>
+    <Card className="filter-expenses">
+      <span className="filter-by-year">
+        <p>Фильтр по годам</p>
+        <form>
+          <select defaultValue={years[0]}>
+            <option>{years[0]}</option>
+            <option>{years[1]}</option>
+            <option>{years[2]}</option>
+            <option>{years[3]}</option>
+          </select>
+        </form>
+      </span>
+      <div className="expense-diagramm">
+        <div className="diagramm-item">
+          <div className="diagramm-item__graph">
+            <div className="diagramm-item__graph__fill"></div>
+          </div>
+          <div className="diagramm-item__month">{months[0]}</div>
+        </div>
+        <div className="diagramm-item">
+          <div className="diagramm-item__graph">
+            <div className="diagramm-item__graph__fill"></div>
+          </div>
+          <div className="diagramm-item__month">{months[1]}</div>
+        </div>
+        <div className="diagramm-item">
+          <div className="diagramm-item__graph">
+            <div className="diagramm-item__graph__fill"></div>
+          </div>
+          <div className="diagramm-item__month">{months[2]}</div>
+        </div>
+        <div className="diagramm-item">
+          <div className="diagramm-item__graph">
+            <div className="diagramm-item__graph__fill"></div>
+          </div>
+          <div className="diagramm-item__month">{months[3]}</div>
+        </div>
+        
+      </div>
+    </Card>
   );
 };
 
