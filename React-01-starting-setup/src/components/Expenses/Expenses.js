bcf82f669eb55a05b0513cbd6659df03c5728be0
CinemaAddict <brokenswords21@gmail.com>
2022-07-31 23:20:50 +0300

    commit

--- React-01-starting-setup/src/components/Expenses/Expenses.js
+++ React-01-starting-setup/src/components/Expenses/Expenses.js
@@ -6,16 +6,29 @@ import "./Expenses.css";
 
 const Expenses = (props) => {
   const [filteredYear, setFilteredYear] = useState("2020");
-  
+
   const filterChangeHandler = (selectedYear) => {
     console.log(selectedYear);
     setFilteredYear(selectedYear);
   };
 
-  const filteredExpenses = props.items.filter(expense => {
+  const filteredExpenses = props.items.filter((expense) => {
     return expense.date.getFullYear().toString() === filteredYear;
   });
-  
+
+let expensesContent = <p>Не найдено расходов в данном периоде.</p>;
+
+if (filteredExpenses.length > 0) {
+  expensesContent = filteredExpenses.map((expense) => (
+    <ExpenseItem
+      key={expense.id}
+      title={expense.title}
+      amount={expense.amount}
+      date={expense.date}
+    />
+  ))
+}
+
   return (
     <div>
       <Card className="expenses">
@@ -23,14 +36,7 @@ const Expenses = (props) => {
           selected={filteredYear}
           onChangeFilter={filterChangeHandler}
         />
-        {filteredExpenses.map((expense) => (
-          <ExpenseItem
-            key={expense.id}
-            title={expense.title}
-            amount={expense.amount}
-            date={expense.date}
-          />
-        ))}
+        {expensesContent}
       </Card>
     </div>
   );
