1eb2ac0304809e7da98901ceadd20b150f724665
CinemaAddict <brokenswords21@gmail.com>
2022-08-04 23:08:48 +0300

    commit

---
+++ React-06-meals-app-practice/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import MenuList from "./components/Menu/MenuList";
+import Header from "./components/UI/Header/Header";
+
+function App() {
+  return (
+    <React.Fragment>
+      <Header></Header>
+      <MenuList></MenuList>
+    </React.Fragment>
+  );
+}
+
+export default App;
---
+++ React-06-meals-app-practice/src/components/UI/Popup/Popup.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Popup = props => {
+    return;
+}
+
+export default Popup;
