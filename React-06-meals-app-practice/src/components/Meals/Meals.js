4dea658a3773dc9853aafe2c524ad76d24d9cd0d
CinemaAddict <brokenswords21@gmail.com>
2022-08-13 18:43:17 +0300

    commit

--- React-06-meals-app-practice/src/components/Meals/Meals.js
+++ React-06-meals-app-practice/src/components/Meals/Meals.js
@@ -3,8 +3,6 @@ import { Fragment } from "react";
 import MealsSummary from "./MealsSummary.js";
 import AvailableMeals from "./AvailableMeals.js";
 
-// import styles from "./Meals.module.css";
-
 const Meals = () => {
   return (
     <Fragment>
