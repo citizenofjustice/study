6453e60b62294435cd793c79941b62df23909ce2
CinemaAddict <brokenswords21@gmail.com>
2023-01-03 19:42:31 +0300

    transitin to the redux toolkit

--- React-11-Redux/src/components/Counter.js
+++ React-11-Redux/src/components/Counter.js
@@ -1,5 +1,5 @@
 import { useSelector, useDispatch } from "react-redux";
-
+import { counterActions } from "../store/index";
 import classes from "./Counter.module.css";
 
 const Counter = () => {
@@ -8,17 +8,17 @@ const Counter = () => {
   const show = useSelector((state) => state.showCounter);
 
   const toggleCounterHandler = () => {
-    dispatch({ type: "toggle" });
+    dispatch(counterActions.toggleCounter());
   };
 
   const incrementHandler = () => {
-    dispatch({ type: "increment" });
+    dispatch(counterActions.increment());
   };
   const decrementHandler = () => {
-    dispatch({ type: "decrement" });
+    dispatch(counterActions.decrement());
   };
   const increaseHandler = () => {
-    dispatch({ type: "increase", amount: 10 });
+    dispatch(counterActions.increase(10));
   };
 
   return (
