08f8ad559fb1dee1e40a256c14b2cfe70003180c
CinemaAddict <brokenswords21@gmail.com>
2023-01-07 18:10:14 +0300

    comment functionality

--- React-14-Router-adv/src/components/comments/Comments.js
+++ React-14-Router-adv/src/components/comments/Comments.js
@@ -1,14 +1,52 @@
-import { useState } from "react";
+import { useState, useEffect, useCallback } from "react";
+import { useParams } from "react-router-dom";
 
-import styles from "./Comments.module.css";
 import NewCommentForm from "./NewCommentForm";
+import CommentsList from "./CommentsList";
+import useHttp from "../../hooks/use-http";
+import { getAllComments } from "../../lib/api";
+import styles from "./Comments.module.css";
+import LoadingSpinner from "../UI/LoadingSpinner";
 
 const Comments = () => {
   const [isAddingComment, setIsAddingComment] = useState(false);
+  const params = useParams();
+  const { sendRequest, status, data: loadedComments } = useHttp(getAllComments);
+
+  const { quoteId } = params;
+
+  useEffect(() => {
+    sendRequest(quoteId);
+  }, [sendRequest, quoteId]);
 
   const startAddCommentHandler = () => {
     setIsAddingComment(true);
   };
+
+  const addedCommentHandler = useCallback(() => {
+    sendRequest(quoteId);
+  }, [sendRequest, quoteId]);
+
+  let comments;
+
+  if (status === "pending") {
+    comments = (
+      <div className="centered">
+        <LoadingSpinner />
+      </div>
+    );
+  }
+
+  if (status === "completed" && loadedComments && loadedComments.length > 0) {
+    comments = <CommentsList comments={loadedComments} />;
+  }
+
+  if (
+    status === "completed" &&
+    (!loadedComments || loadedComments.length === 0)
+  ) {
+    comments = <p className="centered">No comments were added yet...</p>;
+  }
 
   return (
     <section className={styles.comments}>
@@ -18,8 +56,13 @@ const Comments = () => {
           Add a Comment
         </button>
       )}
-      {isAddingComment && <NewCommentForm />}
-      <p>Comments...</p>
+      {isAddingComment && (
+        <NewCommentForm
+          quoteId={quoteId}
+          onAddedComment={addedCommentHandler}
+        />
+      )}
+      {comments}
     </section>
   );
 };
