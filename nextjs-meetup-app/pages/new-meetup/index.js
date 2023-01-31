a53aa03bd611b69f9e8acb08fe6578c8113eb442
CinemaAddict <brokenswords21@gmail.com>
2023-01-31 19:00:54 +0300

    trying to fix error in input form

--- nextjs-meetup-app/pages/new-meetup/index.js
+++ nextjs-meetup-app/pages/new-meetup/index.js
@@ -6,19 +6,23 @@ function NewMeetupPage() {
   const router = useRouter();
 
   async function addMeetupHandler(enteredMeetupData) {
-    const response = await fetch("/api/new-meetup", {
-      method: "POST",
-      body: JSON.stringify(enteredMeetupData),
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
+    try {
+      const response = await fetch("/api/new-meetup", {
+        method: "POST",
+        body: JSON.stringify(enteredMeetupData),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
 
-    const data = await response.json();
+      const data = await response.json();
 
-    console.log(data);
+      console.log(data);
 
-    router.push("/");
+      router.push("/");
+    } catch (error) {
+      throw new Error(error.message);
+    }
   }
 
   return (
