342364321265e9b1186621c0d31c28b907feade1
CinemaAddict <brokenswords21@gmail.com>
2023-06-02 21:43:22 +0300

    inference, functiont types & generics

--- React-18-Typescript/basics.ts
+++ React-18-Typescript/basics.ts
@@ -2,8 +2,7 @@
 // More complex types: arrays, objects
 // Function types, parametrs
 
-// Primitives
-
+/* Primitives */
 let age: number;
 
 age = 12;
@@ -16,16 +15,17 @@ let isInstructor: boolean;
 
 isInstructor = true;
 
-// More complex types
-
+/* More complex types */
 let hobbies: string[];
 
 hobbies = ["Sport", "Cooking"];
 
-let person: {
+type Person = {
   name: string;
   age: number;
 };
+
+let person: Person;
 
 person = {
   name: "Max",
@@ -36,7 +36,33 @@ person = {
 //   isEmployee: true,
 // };
 
-let people: {
-  name: string;
-  age: number;
-}[];
+let people: Person[];
+
+/* Type inference */
+let course: string | number = "React - The Сomplete Guide";
+
+course = 12341;
+
+/* Functions & types */
+
+function add(a: number, b: number): number {
+  return a + b;
+}
+
+function print(value: any) {
+  console.log(value);
+}
+
+/* Generics */
+
+function insertAtBeginning<T>(array: T[], value: T) {
+  const newArray = [value, ...array];
+  return newArray;
+}
+
+const demoArray = [1, 2, 3];
+
+const updatedArray = insertAtBeginning(demoArray, -1); // [-1, 1, 2, 3]
+// updatedArray[0].split("");
+
+const stringArray = insertAtBeginning(["a", "b", "c"], "d");
