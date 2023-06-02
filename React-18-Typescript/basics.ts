38b27c0f02ffc350cc2f1ac39302c9a910ad1a3a
CinemaAddict <brokenswords21@gmail.com>
2023-06-02 21:14:58 +0300

    basics of typescript

---
+++ React-18-Typescript/basics.ts
@@ -0,0 +1,42 @@
+// Primitives: numbers, string, boolean
+// More complex types: arrays, objects
+// Function types, parametrs
+
+// Primitives
+
+let age: number;
+
+age = 12;
+
+let userName: string;
+
+userName = "Max";
+
+let isInstructor: boolean;
+
+isInstructor = true;
+
+// More complex types
+
+let hobbies: string[];
+
+hobbies = ["Sport", "Cooking"];
+
+let person: {
+  name: string;
+  age: number;
+};
+
+person = {
+  name: "Max",
+  age: 32,
+};
+
+// person = {
+//   isEmployee: true,
+// };
+
+let people: {
+  name: string;
+  age: number;
+}[];
