4638a5ff665c7196f3720e79922263bcedea166b
CinemaAddict <brokenswords21@gmail.com>
2023-06-06 23:08:06 +0300

    process

---
+++ NodeJS-01-setup/lessons/process.js
@@ -0,0 +1,23 @@
+/**
+ * config of .env
+ */
+// const dotenv = require("dotenv");
+// dotenv.config();
+
+// console.log(process.env.PORT);
+// console.log(process.env.NODE_ENV);
+
+/**
+ * arguments of node command
+ */
+// console.log(process.argv);
+
+/**
+ * example of exiting a process
+ */
+// if (Math.random() > 0.5) {
+//   while (true) {}
+// } else {
+//   console.log("Выполнение программы завершено");
+//   process.exit();
+// }
