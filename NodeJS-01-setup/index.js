9dc4f6d1bb93f27630ffda227c8a8216226f9908
CinemaAddict <brokenswords21@gmail.com>
2023-06-08 21:56:29 +0300

    creating router

---
+++ NodeJS-01-setup/index.js
@@ -0,0 +1,17 @@
+const PORT = process.env.PORT || 5000;
+
+const Router = require("./framework/Router");
+const Application = require("./framework/Application");
+
+const app = new Application();
+const router = new Router();
+
+router.get("/users", (req, res) => {
+  res.end("YOU SEND REQUEST TO /USERS");
+});
+router.get("/posts", (req, res) => {
+  res.end("YOU SEND REQUEST TO /POSTS");
+});
+
+app.addRouter(router);
+app.listen(PORT, () => console.log(`Server started on PORT ${PORT}`));
