e3a6610ff677e99fe38fac72cc01428719c24932
CinemaAddict <brokenswords21@gmail.com>
2023-06-10 23:09:44 +0300

    connecting mongoDB

--- NodeJS-01-setup/index.js
+++ NodeJS-01-setup/index.js
@@ -1,4 +1,6 @@
 const PORT = process.env.PORT || 5000;
+const mongoose = require("mongoose");
+
 const userRouter = require("./src/user-router");
 const Application = require("./framework/Application");
 const jsonParser = require("./framework/parseJson");
@@ -9,4 +11,16 @@ const app = new Application();
 app.use(jsonParser);
 app.use(parseUrl("http://localhost:5000"));
 app.addRouter(userRouter);
-app.listen(PORT, () => console.log(`Server started on PORT ${PORT}`));
+
+const start = async () => {
+  try {
+    await mongoose.connect(
+      "mongodb+srv://brokenswords21:5l1CuwqcVzL16w0a@cluster0-nodejs-demo.o0ghxht.mongodb.net/?retryWrites=true&w=majority"
+    );
+    app.listen(PORT, () => console.log(`Server started on PORT ${PORT}`));
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+start();
