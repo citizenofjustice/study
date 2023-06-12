b84641d584076eb61757061c055d48734920bdae
CinemaAddict <brokenswords21@gmail.com>
2023-06-12 20:49:16 +0300

    observer for loading posts

--- react-19-repetition/src/pages/Posts.jsx
+++ react-19-repetition/src/pages/Posts.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 
 import { usePosts } from "../hooks/usePost";
 import { useFetching } from "../hooks/useFetching";
@@ -13,6 +13,8 @@ import MyButton from "../components/UI/button/MyButton";
 import Loader from "../components/UI/Loader/Loader";
 import "../styles/App.css";
 import Pagination from "../components/UI/pagination/Pagination";
+import { useObserver } from "../hooks/useObserver";
+import MySelect from "../components/UI/select/MySelect";
 
 function Posts() {
   const [posts, setPosts] = useState([]);
@@ -25,18 +27,23 @@ function Posts() {
   const [limit, setLimit] = useState(10);
   const [page, setPage] = useState(1);
   const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query);
+  const lastElement = useRef();
   const [fetchPosts, isPostLoading, postError] = useFetching(
     async (limit, page) => {
       const response = await PostService.getAll(limit, page);
-      setPosts(response.data);
+      setPosts([...posts, ...response.data]);
       const totalCount = response.headers["x-total-count"];
       setTotalPages(getPageCount(totalCount, limit));
     }
   );
 
+  useObserver(lastElement, page < totalPages, isPostLoading, () => {
+    setPage((prevPage) => prevPage + 1);
+  });
+
   useEffect(() => {
     fetchPosts(limit, page);
-  }, []);
+  }, [page, limit]);
 
   const addNewPost = (post) => {
     setPosts([...posts, { ...post, id: Date.now() }]);
@@ -62,8 +69,19 @@ function Posts() {
       </MyModal>
       <hr style={{ margin: "15px 0" }} />
       <PostFiler filter={filter} setFilter={setFilter} />
+      <MySelect
+        value={limit}
+        onChange={(value) => setLimit(value)}
+        defaultValue="Кол-во элементов на странице"
+        options={[
+          { value: 5, name: "5" },
+          { value: 10, name: "10" },
+          { value: 25, name: "25" },
+          { value: -1, name: "Показать все" },
+        ]}
+      />
       {postError && <h1>Произошла ошибка ${postError}</h1>}
-      {isPostLoading ? (
+      {isPostLoading && (
         <div
           style={{
             display: "flex",
@@ -73,13 +91,13 @@ function Posts() {
         >
           <Loader />
         </div>
-      ) : (
-        <PostList
-          remove={removePost}
-          posts={sortedAndSearchedPosts}
-          title="Посты про JS"
-        />
       )}
+      <PostList
+        remove={removePost}
+        posts={sortedAndSearchedPosts}
+        title="Посты про JS"
+      />
+      <div ref={lastElement} style={{ height: 20, background: "red" }}></div>
       <Pagination totalPages={totalPages} page={page} changePage={changePage} />
     </div>
   );
